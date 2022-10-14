790793b2b382d3954c89c3a31f38431f968e5660
martijnbentum <bentummartijn@gmail.com>
2022-10-14 23:02:46 +0200

    added functions to load audio

--- utils/audio.py
+++ utils/audio.py
@@ -3,6 +3,23 @@ from . import locations
 from . import filename_to_component
 import os
 import subprocess
+import librosa
+
+def load_audio(filename, start = 0.0, end=None):
+	if not end: duration = None
+	else: duration = end - start
+	audio, sr = librosa.load(filename, sr = 16000, offset=start, duration=end)
+	return audio
+
+def load_recording(recording, start = 0.0,end = None):
+	audio = load_audio(recording.wav_filename,start,end)
+	return audio
+	
+
+def load_audio_section(start_time,end_time,filename,audio=None):
+    sampling_rate = 16000
+    if not audio: audio = load_audio(filename)
+    return audio[int(start_time*sampling_rate):int(end_time*sampling_rate)]
 
 def make_audio_filenames_list(restrict_to_dutch = True, save = True):
     fn = glob.glob(locations.cgn_audio_dir +'**', recursive=True)
