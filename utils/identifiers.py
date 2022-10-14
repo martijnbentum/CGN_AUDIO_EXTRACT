f5b095aa5819d8354b443bc8e036688853674aa1
martijnbentum <bentummartijn@gmail.com>
2022-10-14 22:51:37 +0200

    added function to make/load dictonaries mapping components to cgn ids / duration

--- utils/identifiers.py
+++ utils/identifiers.py
@@ -1,5 +1,6 @@
 import os
 from utils import filename_to_component
+import json
 
 cgn_ids_fn = '../cgn_ids'
 speaker_ids_fn = '../speaker_ids'
@@ -45,3 +46,36 @@ def speaker_id_to_speaker(speaker_id):
 
 def cgn_id_to_speakers(cgn_id):
     from text.models import Textgrid, Speaker 
+
+def _make_component_to_cgn_ids_dict():
+    from text.models import Component
+    cgn_ids = load_cgn_ids()
+    components = Component.objects.all()
+    d = {}
+    for cgn_id in cgn_ids:
+        component = cgn_id_to_component(cgn_id)
+        if not component: 
+            print('could not find component of: ',cgn_id)
+            continue
+        if component.name not in d.keys():d[component.name] = []
+        d[component.name].append(cgn_id)
+    with open('../component_to_cgn_ids_dict','w') as fout:
+        json.dump(d,fout) 
+    return d
+
+def component_to_cgn_ids_dict():
+    if not os.path.isfile('../component_to_cgn_ids_dict'): 
+        _make_component_to_cgn_ids_dict()
+    with open('../component_to_cgn_ids_dict') as fin:
+        d = json.load(fin)
+    return d
+
+def component_to_duration_dict():
+    from text.models import Component
+    d = {}
+    for c in Component.objects.all():
+        d[c.name] = sum([a.duration for a in c.audio_set.all()])
+    return d
+        
+    
+
