4b78817ad6fc1d42754d73482946b44d32e0d684
martijnbentum <bentummartijn@gmail.com>
2022-10-14 18:40:58 +0200

    added methods on textgrid to extract phrases from the textgrid; added word_ipa_phoneme and ipa field to word, added sampa_phonemes and duration properties

--- text/models.py
+++ text/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import textgrids
+from utils import word
 
 
 # Create your models here.
@@ -64,12 +65,43 @@ class Textgrid(models.Model):
     def load_awd(self):
         return textgrids.TextGrid(self.awd_filename)
 
+    def speaker_to_words_dict(self):
+        if hasattr(self,'_speaker_to_words_dict'): 
+            return self._speaker_to_words_dict
+        d = {}
+        for speaker in self.speakers.all():
+            d[speaker.speaker_id] = self.word_set.filter(speaker = speaker)
+        self._speaker_to_words_dict = d
+        return self._speaker_to_words_dict  
+                     
+    def speaker_to_phrases_dict(self, end_on_eos = True, minimum_duration=None,
+        maximum_duration=None):
+        d = self.speaker_to_words_dict()
+        od = {}
+        for speaker_id, words in d.items():
+            od[speaker_id] = word.words_to_phrases(words,end_on_eos,
+                minimum_duration,maximum_duration)
+        self._speaker_words_to_phrases_dict = od
+        return self._speaker_words_to_phrases_dict
+
+
+    def phrases(self, end_on_eos = True, minimum_duration = None,
+        maximum_duration= None):
+        d = self.speaker_to_phrases_dict(end_on_eos,minimum_duration,
+            maximum_duration)
+        o = []
+        for phrases in d.values():
+            o.extend(phrases)
+        return o
+            
 
 class Word(models.Model):
     dargs = {'on_delete':models.SET_NULL,'blank':True,'null':True}
     word = models.CharField(max_length=100,default=None,null=True)
     word_phoneme = models.CharField(max_length=100,default=None,null=True)
     phonemes = models.TextField(default='')
+    word_ipa_phoneme = models.CharField(max_length=100,default=None,null=True)
+    ipa = models.CharField(max_length=100,default=None,null=True)
     start_time = models.FloatField(default = None,null=True)
     end_time = models.FloatField(default = None,null=True)
     textgrid= models.ForeignKey(Textgrid,**dargs)
@@ -81,9 +113,19 @@ class Word(models.Model):
 
     def __str__(self):
         m = self.word + ' | ' + self.word_phoneme
+        m = self.word + ' | ' + self.word_ipa_phoneme
         m += ' | ' + str(round(self.end_time - self.start_time,2))
         return m
 
     class Meta:
         unique_together = ('textgrid','speaker','awd_word_tier_index')
+
+    @property
+    def sampa_phonemes(self):
+        p = eval(self.phonemes).values()
+        return [x.split('\t')[0].strip('=') for x in p]
+
+    @property
+    def duration(self):
+        return self.end_time - self.start_time
 
