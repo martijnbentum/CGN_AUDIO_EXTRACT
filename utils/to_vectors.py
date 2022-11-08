40a08ca6eb7f135fd26a1c3b8257f8138ed0301e
martijnbentum <bentummartijn@gmail.com>
2022-11-08 14:31:11 +0100

    added function to load non ctc model and generate outputs with non ctc model

--- utils/to_vectors.py
+++ utils/to_vectors.py
@@ -4,32 +4,58 @@ import torch
 import numpy as np
 from datasets import load_dataset
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor, Wav2Vec2CTCTokenizer
+from transformers import Wav2Vec2Model
 from .wav2vec2_model import load_vocab
 from . import audio
+from . import locations
 
 repo_dir = '/vol/tensusers/mbentum/CGN_AUDIO_EXTRACT/repo/'
 default_checkpoint = repo_dir + 'o_first_test/checkpoint-6300'
 
+pretrained_small="facebook/wav2vec2-xls-r-300m"
+pretrained_big ="facebook/wav2vec2-xls-r-2b"
 
-def checkpoint_to_processor_and_model(checkpoint):
+def checkpoint_to_processor_and_ctc_model(checkpoint):
     processor = Wav2Vec2Processor.from_pretrained(checkpoint)
     model =Wav2Vec2ForCTC.from_pretrained(checkpoint)
     return processor, model
 
+def load_pretrained_processor_model(version = 'small'):
+    '''
+    # does not work
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-xls-r-2b" ,
+        cache_dir = locations.cache_dir)
+    '''
+    if version == 'small': name = pretrained_small
+    else: name = pretrained_big
+    # loading a non specific processor do not know whether this is ok
+    processor = Wav2Vec2Processor.from_pretrained(default_checkpoint)
+    model = Wav2Vec2Model.from_pretrained(name,
+        cache_dir = locations.cache_dir)
+    return processor, model
 
-def audio_to_outputs(audio_filename, start=None, end=None, processor=None, 
+def audio_to_ctc_outputs(audio_filename, start=None, end=None, processor=None, 
     model=None):
     if not processor or not model: 
         print('loading default checkpoint trained cgn o:',default_checkpoint)
         processor, model = checkpoint_to_processor_and_model(default_checkpoint)
-    if not start or not end: 
-        array = audio.load_audio_section(start,end,audio_filename)
-    else:
-        array = audio.load_audio(audio_filename)
+    array = audio.load_audio(audio_filename,start, end)
     inputs = processor(array, sampling_rate=16_000, return_tensors='pt',
         padding= True)
-    outputs = model(inputs.input_values, attention_mask=inputs.attention_mask,
-        output_hidden_states = True)
+    with torch.no_grad():
+        outputs = model(**inputs,output_hidden_states = True)
+    return outputs
+
+def audio_to_pretrained_outputs(audio_filename, start=None, end=None, 
+    processor=None, model=None):
+    if not processor or not model: 
+        print('loading pretrained model: facebook/wav2vec2-xls-r-2b')
+        processor, model = load_pretrained_processor_model()
+    array = audio.load_audio(audio_filename,start, end)
+    inputs = processor(array, sampling_rate=16_000, return_tensors='pt',
+        padding= True)
+    with torch.no_grad():
+        outputs = model(**inputs,output_hidden_states = True)
     return outputs
     
 
