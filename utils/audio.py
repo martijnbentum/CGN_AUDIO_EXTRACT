56a5f31fb6f1bb7d127383a90425796481141168
martijnbentum <bentummartijn@gmail.com>
2022-11-08 11:06:21 +0100

    bug fix load_audio function

--- utils/audio.py
+++ utils/audio.py
@@ -8,7 +8,7 @@ import librosa
 def load_audio(filename, start = 0.0, end=None):
 	if not end: duration = None
 	else: duration = end - start
-	audio, sr = librosa.load(filename, sr = 16000, offset=start, duration=end)
+	audio, sr = librosa.load(filename, sr = 16000, offset=start, duration=duration)
 	return audio
 
 def load_recording(recording, start = 0.0,end = None):
