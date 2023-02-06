8b7947d2524f3b4cacba7e1b71e22e5c7f54e5b1
martijnbentum <bentummartijn@gmail.com>
2023-02-06 16:52:46 +0100

    added new location for hidden states

--- utils/locations.py
+++ utils/locations.py
@@ -14,6 +14,8 @@ syllable_dir= '../SYLLABLE/'
 timestamps_dir='../O_PHONEMES_TIMESTAMPS/'
 ctc_hidden_states_dir='../O_CTC_HIDDEN_STATES/'
 hidden_states_dir='../O_HIDDEN_STATES/'
+new_ctc_hidden_states_dir='/vol/tensusers3/mbentum/O_CTC_HIDDEN_STATES/'
+new_hidden_states_dir='/vol/tensusers3/mbentum/O_HIDDEN_STATES/'
 
 cache_dir = '../WAV2VEC_DATA/'
 perceptron_dir = '../PERCEPTRONS/'
