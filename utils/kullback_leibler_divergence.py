1f8548abfec6380a527bab047886dc39addb25fd
martijnbentum <bentummartijn@gmail.com>
2023-02-17 17:35:11 +0100

    added some methods to show phoneme prob vector for a given frame; added a function to create kl audios for all files in a cgn component; kl audio now also contains a reference to all kl frames

--- utils/kullback_leibler_divergence.py
+++ utils/kullback_leibler_divergence.py
@@ -3,9 +3,28 @@ from utils import perceptron
 from utils import phonemes
 from utils import to_vectors
 from utils import identifiers
+from utils import locations
 from text.models import Textgrid
 import numpy as np
+import os
+import pickle
 
+def cgn_id_to_kl_audio_filename(cgn_id):
+    f = locations.kl_audio_dir + cgn_id + '_kl.pickle'
+    return f
+
+def make_kl_audios_for_cgn_component(component, overwrite = False):
+    t = Textgrid.objects.filter(component__name = component)
+    for x in t:
+        filename = cgn_id_to_kl_audio_filename(x.cgn_id)
+        print('\n','-'*90,'\nhandling',filename,'\n','-'*90,'\n' )
+        if os.path.isfile(filename) and not overwrite: 
+            print('already done, skipping')
+            continue
+        kla = KLAudio(x.cgn_id)
+        with open(filename, 'wb') as fout:
+            pickle.dump(kla,fout)
+        del kla
 
 def get_vocabs():
     '''get the vocabs to get the indices of the phonemes 
@@ -56,13 +75,14 @@ def compute_mlp_output_and_synthetic_bpc(matrix):
     for i,phoneme in enumerate(phoneme_labels):
         try:bpc = bpcs.find_bpc(phoneme)
         except: 
-            output.append([phoneme,i,None])
+            output.append([phoneme,i,None,None,None])
             continue
         spd = bpc.synthetic_probability_ditribution(phoneme)
         indices = synthetic_probability_ditribution_to_indices(spd)
+        phons = get_phonemes(indices)
         p = normalize_prob_vector(matrix[i,indices])
         q = list(spd.values())
-        output.append([phoneme,i,kl_divergence(p,q)])
+        output.append([phoneme,i,kl_divergence(p,q), p, phons])
     return output
 
 def kl_divergence(p,q):
@@ -71,12 +91,24 @@ def kl_divergence(p,q):
     q   model probs
     '''
     return np.sum(p*np.log(p/q))
+
+def get_phonemes(indices):
+    phons = ''
+    for i in indices:
+        phons+= ipa_reverse_vocab[i]
+    return phons
+
         
 class KLAudio:
     def __init__(self, cgn_id):
         self.cgn_id = cgn_id
         self.component = identifiers.cgn_id_to_component(self.cgn_id).name
         self.compute_klphrases()
+
+    def __repr__(self):
+        m = self.cgn_id + ' '
+        m += 'nphrases: ' + str(len(self.klphrases_ctc))
+        return m
 
     def compute_klphrases(self):
         textgrid = Textgrid.objects.get(cgn_id = self.cgn_id)
@@ -87,9 +119,28 @@ class KLAudio:
             end_time = phrase[-1].end_time
             x = [textgrid.audio.filename,index,start_time,end_time]
             self.klphrases_pretrained.append( KLPhrase(*x,ctc=False) )
-            self.klphrases_pretrained.append( KLPhrase(*x,ctc=True) )
-        
-        
+            self.klphrases_ctc.append( KLPhrase(*x,ctc=True) )
+
+    @property
+    def klframes_pretrained(self):
+        return self._get_frames('_pretrained')
+
+    @property
+    def klframes_ctc(self):
+        return self._get_frames('_ctc')
+
+    def _get_frames(self,frame_type):
+        if hasattr(self,'_klframes_' + frame_type):
+            return getattr(self,'_klframes_' + frame_type)
+        if 'pretrained' in frame_type:phrases = self.klphrases_pretrained
+        else: phrases = self.klphrases_ctc
+        output = []
+        for x in phrases:
+            for frame in x.klframes:
+                if not frame.kl: continue
+                output.append(frame)
+        setattr(self,'_klframes_'+frame_type, output)
+        return getattr(self,'_klframes_' + frame_type)
 
 class KLPhrase:
     def __init__(self,audio_filename, index, start_time, end_time, ctc):
@@ -122,17 +173,19 @@ class KLPhrase:
         matrix = clf.predict_proba(x)
         output = compute_mlp_output_and_synthetic_bpc(matrix)
         for line in output:
-            phoneme, i, kl = line
-            klf = KLFrame(phoneme, i, kl, layer, self.ctc)
+            phoneme, i, kl, probs, phons = line
+            klf = KLFrame(phoneme, i, kl, probs, phons,layer, self.ctc)
             self.klframe_layer_dict[layer].append(klf)
             self.klframes.append(klf)
         
     
 class KLFrame:
-    def __init__(self, phoneme, i, kl, layer, ctc):
+    def __init__(self, phoneme, i, kl,probs,phons, layer, ctc):
         self.phoneme = phoneme
         self.frame_index = i
         self.kl = kl
+        self.probability_vector = probs
+        self.phonemes_vector = phons
         self.layer = layer
         self.ctc = ctc
 
@@ -143,5 +196,18 @@ class KLFrame:
         m += str(self.layer) + ' ' + model_type
         return m
 
+    @property
+    def phoneme_probability_vector(self):
+        return sorted_phoneme_probs(self.phonemes_vector,
+            self.probability_vector)
 
     
+def sorted_phoneme_probs(phonemes_vector,probability_vector):
+    output = []
+    for phon, prob in zip(phonemes_vector,probability_vector):
+        output.append([phon,prob])
+    output = sorted(output, key = lambda x: x[1], reverse = True)
+    return output
+        
+
+
