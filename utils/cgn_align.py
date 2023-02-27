3517153c94c551ce499bdbfb5bf0480c12dca7dc
martijnbentum <bentummartijn@gmail.com>
2023-02-27 10:46:02 +0100

    added code to align randomized cgn text to see impact of transcription noise

--- utils/cgn_align.py
+++ utils/cgn_align.py
@@ -8,8 +8,17 @@ import random
 from utils import needleman_wunch as nw
 from text.models import Textgrid
 
+
 cgn_wav2vec_dir = '/vol/tensusers3/mbentum/cgn_wav2vec/'
 cgn_align = '/vol/tensusers3/mbentum/cgn_wav2vec_align/'
+
+def _select_10_perc_table_filenames():
+    random.seed(9)
+    fn = table_filenames()
+    random.shuffle(fn)
+    nfiles = int(len(fn)/10)
+    return fn[:nfiles]
+    
 
 def table_filenames():
     fn = glob.glob(cgn_wav2vec_dir + '*.table')
@@ -37,26 +46,59 @@ def table_filename_to_cgn_id(f):
     cgn_id = f.split('/')[-1].split('.')[0]
     return cgn_id
 
-def make_alignments():
-    fn = table_filenames()
+def make_alignments(small_set = False, randomize = None):
+    if not small_set: fn = table_filenames()
+    else: fn = _select_10_perc_table_filenames()
     random.shuffle(fn)
     for f in fn:
         cgn_id = table_filename_to_cgn_id(f)
-        align_filename = cgn_align + cgn_id
+        if randomize:
+            directory = cgn_align[:-1] + '_' + str(randomize) + '/'
+        else: 
+            directory = cgn_align
+        align_filename = directory+ cgn_id
         if os.path.isfile(align_filename): 
-            print('skiping',cgn_id)
+            print('skiping',cgn_id, small_set, randomize, len(fn))
             continue
-        print('handling',cgn_id)
-        Align(cgn_id, make_phrases = False)
+        print('handling',cgn_id, small_set, randomize, len(fn))
+        Align(cgn_id, make_phrases = False, randomize = randomize)
+
+def make_all_randomized_alignments():
+    rv = [2,4,8,16,32,64]
+    random.shuffle(rv)
+    print(rv)
+    for r in rv:
+        print('-'*80)
+        print('start with:',r)
+        print('-'*80)
+        make_alignments(small_set = True,randomize = r)
+
         
-    
+def randomize_text(text, random_perc):
+    nchar = len(text)
+    chars = set(text)
+    if nchar < 2: return text
+    if random_perc > 1: random_perc /= 100
+    sample = int(nchar * random_perc)
+    if sample == 0: sample = 1
+    print(nchar,sample,random_perc)
+    indices = random.sample(list(range(nchar)), sample)
+    output = ''
+    for i, char in enumerate(text):
+        if i in indices: 
+            c = random.sample(list(chars - set(char)), 1)[0]
+            output += c
+        else: output += char
+    return output
+
 
     
 class Align:
     '''align object to align cgn textgrid and wav2vec transcript.
     '''
-    def __init__(self,cgn_id, make_phrases = True):
+    def __init__(self,cgn_id, make_phrases = True, randomize = None):
         self.cgn_id = cgn_id
+        self.randomize = randomize
         self.textgrid = Textgrid.objects.get(cgn_id = cgn_id)
         self.awd_words = list(self.textgrid.word_set.all())
         # self.awd_text = ' '.join([w.awd_word for w in self.awd_words])
@@ -73,13 +115,20 @@ class Align:
         self.wav2vec_text = load_text(self.wav2vec_base_filename+'.txt')
 
     def _set_align(self):
-        self.align_filename = cgn_align + self.cgn_id
+        if self.randomize: 
+            directory = cgn_align[:-1] + '_' + str(self.randomize) + '/'
+            if not os.path.isdir(directory): os.mkdir(directory)
+            cgn_text = randomize_text(self.awd_text.lower(), self.randomize)
+        else:
+            directory = cgn_align
+            cgn_text = self.awd_text
+        self.align_filename = directory + self.cgn_id
         print(self.align_filename)
         if os.path.isfile(self.align_filename): 
             with open(self.align_filename) as fin:
                 self.align = fin.read()
         else:
-            self.align = nw.nw(self.awd_text, self.wav2vec_text)
+            self.align = nw.nw(cgn_text, self.wav2vec_text)
             with open(self.align_filename, 'w') as fout:
                 fout.write(self.align)
         
