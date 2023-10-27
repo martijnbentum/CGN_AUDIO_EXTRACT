c29f76c463f3b4a9b5350a00dc84dc9db6bcc931
martijnbentum <bentummartijn@gmail.com>
2023-10-27 13:01:05 +0200

    paths are now stored in locations module; renamed functions to be more general

--- ld/words_to_vectors.py
+++ ld/words_to_vectors.py
@@ -9,26 +9,25 @@ MALD words.
 
 import glob
 from utils import to_vectors
+from utils import locations
 
-
-input_directory = '/vol/tensusers/mbentum/variable_stress_syllable/wav_16khz/'
-output_directory = '/vol/tensusers/mbentum/variable_stress_syllable/'
-output_directory += 'pretrain_wav2vec_vectors/'
+input_directory = locations.mald_variable_stress_wav
+output_directory = locations.mald_variable_stress_pretrain_vectors
 
 def get_name(filename):
     '''remove directory and extension from filename'''
     return filename.split('/')[-1].split('.')[0]
 
-def handle_audio_files(input_directory = input_directory, 
+def all_audio_files_to_pretrain_vectors(input_directory = input_directory, 
     output_directory = output_directory, processor = None, model = None):
     '''uses pretrained model to convert audio files to vectors'''
     if not processor:
         processor, model = to_vectors.load_pretrained_model()
     files = glob.glob(directory + '*.wav')
     for f in files:
-        handle_audio_file(f, processor, model)
+        audio_file_to_vector(f, processor, model)
 
-def handle_audio_file(filename, processor, model, output_directory = ''):
+def audio_file_to_vector(filename, processor, model, output_directory = ''):
     '''converts audio file to vectors and saves them to a pickle file'''
     print('handling file: ' + filename)
     name = get_filename(filename)
