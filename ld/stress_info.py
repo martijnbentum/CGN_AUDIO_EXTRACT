90402b7b5f6ae4c58ad19e34a440249df600e480
martijnbentum <bentummartijn@gmail.com>
2023-11-03 15:36:31 +0100

    occlusion version of pretrained hidden states (based on audio files with only vowel or syllable audio other material set to silence

--- ld/stress_info.py
+++ ld/stress_info.py
@@ -17,6 +17,8 @@ class Info:
             self.variable_stress_wav_dir = locations.mald_variable_stress_wav 
             path = locations.mald_variable_stress_pretrain_vectors 
             self.variable_stress_pretrain_vectors_dir = path
+            path = locations.mald_variable_stress_occlusions_pretrain_vectors
+            self.variable_stress_occlusions_pretrain_vectors_dir = path
         else: raise ValueError('model_type must be wav2vec')
         self._set_info()
 
@@ -29,11 +31,13 @@ class Info:
         self.data = temp[1:]
         self.syllables = [Syllable(x, self.header,self) for x in self.data]
 
-    def xy(self, layer='cnn', section = 'syllable', random_gt = False):
+    def xy(self, layer='cnn', section = 'syllable', random_gt = False,
+        occlusion_type = None):
         attr_name = '_xy_' + section + '_' + str(layer)
         if hasattr(self, attr_name):
             return getattr(self, attr_name)
-        X = np.array([x.X(layer, section) for x in self.syllables])
+        ot = occlusion_type
+        X = np.array([x.X(layer, section, ot) for x in self.syllables])
         y = np.array([x.y(random_gt) for x in self.syllables])
         setattr(self, attr_name, (X,y))
         return getattr(self, attr_name)
@@ -65,12 +69,31 @@ class Syllable:
         return f
 
     @property
-    def pretrain_vectors(self):
-        if hasattr(self, '_pretrain_vectors'):
-            return self._pretrain_vectors
-        with open(self.pretrain_vectors_filename, 'rb') as f:
-            self._pretrain_vectors = pickle.load(f)
-        return self._pretrain_vectors
+    def occlusions_pretrain_vectors_filename_vowel(self):
+        f = self.info.variable_stress_occlusions_pretrain_vectors_dir 
+        f += self.name + '_only_vowel.pickle'
+        return f
+
+    @property
+    def occlusions_pretrain_vectors_filename_syllable(self):
+        f = self.info.variable_stress_occlusions_pretrain_vectors_dir 
+        f += self.name + '_only_syllable.pickle'
+        return f
+
+    def pretrain_vectors(self, occlusion_type = None):
+        if not occlusion_type:attr_name = '_pretrain_vectors'
+        else: attr_name = '_pretrain_vectors_' + str(occlusion_type)
+        if hasattr(self, attr_name):
+            return getattr(self,attr_name)
+        if occlusion_type: 
+            name = 'occlusions_pretrain_vectors_filename_'
+            filename = getattr(self,name + occlusion_type)
+        else: filename = self.pretrain_vectors_filename
+
+        with open(filename, 'rb') as f:
+            vectors = pickle.load(f)
+        setattr(self, attr_name, vectors)
+        return getattr(self,attr_name)
 
     @property
     def sox_info(self):
@@ -112,15 +135,17 @@ class Syllable:
     def vowel_start_end_time(self):
         return self.vowel_start_time, self.vowel_end_time
 
-    def _get_feature_vectors(self, layer = 'cnn'):
+    def _get_feature_vectors(self, layer = 'cnn', occlusion_type = None):
+        pretrain_vectors = self.pretrain_vectors(occlusion_type)
         if layer == 'cnn':
-            return self.pretrain_vectors.extract_features[0].numpy()
+            return pretrain_vectors.extract_features[0].numpy()
         if type(layer) != int:
             raise ValueError('layer must be layer index or "cnn"')
-        return self.pretrain_vectors.hidden_states[layer][0].numpy()
+        return pretrain_vectors.hidden_states[layer][0].numpy()
 
-    def feature_vectors(self, layer = 'cnn', section = 'syllable'):
-        feature_vectors = self._get_feature_vectors(layer)
+    def feature_vectors(self, layer = 'cnn', section = 'syllable',
+        occlusion_type = None):
+        feature_vectors = self._get_feature_vectors(layer, occlusion_type)
         if section == 'syllable':
             start_index, end_index = self.start_end_index
         elif section == 'vowel':
@@ -129,16 +154,18 @@ class Syllable:
             start_index, end_index = 0, feature_vectors.shape[0]
         return feature_vectors[start_index:end_index]
 
-    def mean_feature_vector(self, layer = 'cnn', section = 'syllable'):
+    def mean_feature_vector(self, layer = 'cnn', section = 'syllable',
+        occlusion_type = None):
         attr_name = '_' + section + '_mean_feature_vector_' + str(layer)
         if hasattr(self,attr_name):
             return getattr(self,attr_name)
-        temp = np.mean(self.feature_vectors(layer, section), axis=0)
+        temp = np.mean(self.feature_vectors(layer, section, occlusion_type), 
+            axis=0)
         setattr(self, attr_name, temp)
         return getattr(self,attr_name)
     
-    def X(self, layer = 'cnn', section = 'syllable'):
-        return self.mean_feature_vector(layer, section)
+    def X(self, layer = 'cnn', section = 'syllable', occlusion_type = None):
+        return self.mean_feature_vector(layer, section, occlusion_type)
 
     def y(self, random_gt = False):
         if random_gt: return random.randint(0,1)
