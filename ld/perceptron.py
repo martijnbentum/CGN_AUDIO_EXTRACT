c9c27d1e572d3ba39f3c0b8234b489ca43e86b33
martijnbentum <bentummartijn@gmail.com>
2024-01-03 14:15:37 +0100

    added code to train many classifiers with different data split to test whether cnn and transformer differ between non ocluded and ocluded audio input

--- ld/perceptron.py
+++ ld/perceptron.py
@@ -14,11 +14,26 @@ layers = ['cnn',1,6,12,18,21,24]
 sections = ['vowel', 'syllable', 'word']
 
 def train_classifier(stress_info, name , layer, section, overwrite = False,
-    random_gt = False, occlusion_type = None):
+    random_gt = False, occlusion_type = None, random_state = 1):
+    '''train mlp classifier based on the data structure hidden_states.
+    name            info for in the filename (wav2vec model pretrained)
+    layer           layer of wav2vec 2.0 to use
+    section         section of the data to use (vowel, syllable, word)
+    overwrite       if True, overwrite existing files
+    random_gt       if True, use random ground truth (sanity check)
+    occlusion_type  if not None, use occluded data (all audio outside
+                    vowel or syllable is set to 0)
+    random_state    random state for train_test_split
+                    used for the ccn tf (transformer) comparison
+    '''
     if name: name = '_' + name
     if random_gt: name += '-random-gt'
     if occlusion_type: name += '-occlusion-' + occlusion_type
-    f=locations.stress_perceptron_dir + 'clf' + name + '_' + section   
+    if random_state != 1: 
+        name += '_rs-' + str(random_state)
+        f=locations.stress_perceptron_dir + 'clf' + name + '_' + section   
+    else:
+        f=locations.stress_perceptron_dir + 'clf' + name + '_' + section   
     f+= '_' + str(layer) + '.pickle'
     if os.path.isfile(f) and not overwrite:
         print(f, 'already exists, skipping')
@@ -27,23 +42,26 @@ def train_classifier(stress_info, name , layer, section, overwrite = False,
     X, y = stress_info.xy(layer = layer, section = section, 
         random_gt = random_gt, occlusion_type = occlusion_type)
     X_train, X_test, y_train, y_test = train_test_split(X, y, 
-        stratify=y,random_state=1)
+        stratify=y,random_state= random_state)
     clf=MLPClassifier(random_state=1,max_iter=300)
     clf.fit(X_train, y_train)
     hyp = clf.predict(X_test)
-    save_performance(y_test, hyp, name, layer, section)
+    save_performance(y_test, hyp, name, layer, section, random_state)
+        
     with open(f, 'wb') as fout:
         pickle.dump(clf,fout)
 
 def train_classifiers(stress_info, name = '', layers = layers, 
-    sections = sections, random_gt = False, occlusion_type = None):
+    sections = sections, random_gt = False, occlusion_type = None,
+    random_state = 1):
     '''train mlp classifiers based on the data structure hidden_states.'''
     for layer in layers:
         for section in sections:
             train_classifier(stress_info, name, layer, section, 
-                random_gt = random_gt, occlusion_type = occlusion_type)
+                random_gt = random_gt, occlusion_type = occlusion_type,
+                random_state = random_state)
 
-def save_performance(gt, hyp, name, layer, section):
+def save_performance(gt, hyp, name, layer, section, random_state):
     d = {}
     d['mcc'] = round(matthews_corrcoef(gt, hyp), 3)
     d['accuracy'] = round(accuracy_score(gt, hyp), 3)
@@ -52,11 +70,22 @@ def save_performance(gt, hyp, name, layer, section):
     for k,v in d.items():
         print(k,v)
     print('---')
-    f = locations.stress_perceptron_dir + 'score' + name 
-    f +=  '_' + str(layer)+'_'+ section + '.json'
+    if random_state != 1: 
+        rs = '_rs-' + str(random_state)
+        f = locations.cnn_tf_comparison_dir + 'score' + name 
+    else: 
+        rs = ''
+        f = locations.stress_perceptron_dir + 'score' + name 
+    f +=  '_' + str(layer)+'_'+ section + rs + '.json'
     with open(f, 'w') as fout:
         json.dump(d, fout)
     return d
+
+def train_mlp_for_cnn_tf_comparison(stress_info, name, layers = None,
+    n_classifiers = 100):
+    if not layers: layers = layers
+    for i in range(2,n_classifiers + 2):
+        train_classifiers(stress_info, name, layers, random_state = i)
 
 
 class Perceptron:
