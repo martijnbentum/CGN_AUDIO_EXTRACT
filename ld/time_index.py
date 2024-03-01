8c06b026f2d28d145aeb154d1958c818c4a59bb4
martijnbentum <bentummartijn@gmail.com>
2024-03-01 13:35:29 +0100

    added nudge function to find whether the start or end frame index needs to be changed when start == end index

--- ld/time_index.py
+++ ld/time_index.py
@@ -19,6 +19,20 @@ def end_time_to_index(time, frame_duration = 0.02, greedy = False):
     if start_time > time: return index - 1
     return index
 
+def _find_nudge_index(start_index, end_index, start_time, end_time,
+    frame_duration):
+    '''find the index that is farthest from the time slice of a frame
+    and nudge it to the closest index that is within the time slice of a frame
+    '''
+    check_start,check_end = index_slice_to_time_slice(start_index,
+        end_index, frame_duration)
+    start_diff = check_start - start_time
+    end_diff = end_time - check_end
+    if start_diff > end_diff:
+        return start_index - 1, end_index
+    return start_index, end_index + 1
+    
+
 def time_slice_to_index_slice(start_time,end_time, frame_duration = 0.02):
     '''find the index tuple that will slice those vectors that
     falls within the time slice
@@ -29,7 +43,13 @@ def time_slice_to_index_slice(start_time,end_time, frame_duration = 0.02):
             raise ValueError("start",start_time,"end",end_time, "bad")
     start_index = start_time_to_index(start_time, frame_duration)
     end_index = end_time_to_index(end_time, frame_duration)
-    if start_index == end_index: end_index += 1
+    nudged = False
+    if start_index == end_index: 
+        # if the start and end index is the same, then nudge the index
+        # that is farthest from the time slice of a frame
+        start_index, end_index = _find_nudge_index(start_index, end_index,
+            start_time, end_time, frame_duration)
+        nudged = True
     if start_index > end_index: 
         if start_index - end_index > 1: 
             raise ValueError("start",start_index,"end",end_index, "bad")
@@ -40,7 +60,7 @@ def time_slice_to_index_slice(start_time,end_time, frame_duration = 0.02):
     duration = end_time - start_time
     # if the duration of the time slice is shorter than the frame duration
     # the index slice will not be contained witin the time slice
-    if duration > frame_duration * 1.5:
+    if duration > frame_duration * 1.5 and not nudged:
         # if duration is longer than frame duration
         # the check_start should be later or equal than the start time
         # and the check_end should be earlier or equal than the end time
